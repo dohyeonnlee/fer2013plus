db02b8a8b2ec9d5edee5ef071011f8769e6225c7
Sohyun-y <147599743+Sohyun-y@users.noreply.github.com>
2024-04-05 17:19:04 +0900

    Update model.py

--- model.py
+++ model.py
@@ -11,3 +11,15 @@ class myVGG19(nn.Module):
     
     def forward(self, x):
         return self.vgg19(x)
+
+
+class myVGG16(nn.Module):
+    def __init__(self):
+        super(myVGG16, self).__init__()
+        self.vgg16 = models.vgg16(pretrained=False)
+        self.vgg16.features[0] = nn.Conv2d(1, 64, kernel_size=3, padding=1)  # 첫 번째 레이어를 1채널 입력에 맞게 수정
+        num_ftrs = self.vgg16.classifier[6].in_features
+        self.vgg16.classifier[6] = nn.Linear(num_ftrs, 10)  # 마지막 레이어를 10개 클래스에 맞게 조정
+    
+    def forward(self, x):
+        return self.vgg16(x)
